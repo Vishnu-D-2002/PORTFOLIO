2e94be0d2c2a1b910f9901cb26169e3d6958feba
Vishnu-D-2002 <vishnulosangels@gmail.com>
2023-12-08 01:14:06 +0530

    img

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -8,7 +8,7 @@ function Intro() {
         I'm VISHNU.D FULL STACK DEVELOPER
       </div>
       <div id='end'>
-        <img src='../../src/assets/vishnu.jpg' alt='My Image' className="rounded-image" />
+        <img src='../src/assets/vishnu.jpg' alt='My Image' className="rounded-image" />
       </div>
       
     </div>
