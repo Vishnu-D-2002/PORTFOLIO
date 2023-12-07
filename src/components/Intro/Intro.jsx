ac3a5769742be04458dd2987f97d8077db2b156a
Vishnu-D-2002 <vishnulosangels@gmail.com>
2023-12-08 00:38:26 +0530

    img rounded

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import './Intro.css'; 
 
 function Intro() {
   return (
-    <div>Intro</div>
+    <div>
+      <div id='left'>
+        I'm VISHNU.D FULL STACK DEVELOPER
+      </div>
+      <div id='end'>
+        <img src='src\assets\vishnu.jpg' alt='My Image' className="rounded-image" />
+      </div>
+      
+    </div>
   )
 }
 
-export default Intro
+export default Intro;
