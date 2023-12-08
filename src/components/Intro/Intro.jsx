945789a5d476efdef32c98c3e9c5200d78b0e26b
Vishnu-D-2002 <vishnulosangels@gmail.com>
2023-12-08 20:20:09 +0530

    block in small

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -10,7 +10,7 @@ function Intro() {
     text.split('').forEach((letter, index) => {
       setTimeout(() => {
         setText(prevText => prevText + letter);
-      }, index * 300);
+      }, index * 500);
     });
   };
 
@@ -21,7 +21,7 @@ function Intro() {
     const intervalId = setInterval(() => {
       animateTitle("VISHNU", setTitleText);
       animateTitle('FULL STACK DEVELOPER (MERN)', setFsdText);
-    }, 9000);
+    }, 14000);
 
     return () => clearInterval(intervalId);
   }, []);
@@ -31,6 +31,9 @@ function Intro() {
       <div id='left-content'>
         <h1 id='title'>Hi, I'm {titleText}</h1><br />
         <h1 id='fsd'>{fsdText}</h1>
+        <p id='para'>
+            I'm a Full Stack Developer with expertise in creating websites and apps using a technology stack called MERN (MongoDB, Express.js, React, Node.js). I work on both the visible parts and the behind-the-scenes parts. My goal is to make user-friendly and efficient applications by writing smart and organized code. I enjoy learning about new technologies and collaborating with teams to build innovative solutions. Let's create something awesome together!
+          </p>
       </div>
       <div id='right-content'>
         <div id='end'>
