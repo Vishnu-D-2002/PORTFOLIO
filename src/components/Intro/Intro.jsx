f31ad010eb9777f966dc629c4016f71fe9646f39
Vishnu-D-2002 <vishnulosangels@gmail.com>
2023-12-08 19:32:54 +0530

    stated

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import './Intro.css';
 
 function Intro() {
-  const [titleText, setTitleText] = useState("Hi, I'm VISHNU");
+  const [titleText, setTitleText] = useState("VISHNU");
   const [fsdText, setFsdText] = useState("FULL STACK DEVELOPER (MERN)");
 
   const animateTitle = (text, setText) => {
@@ -15,11 +15,11 @@ function Intro() {
   };
 
   useEffect(() => {
-    animateTitle("Hi, I'm VISHNU", setTitleText);
+    animateTitle("VISHNU", setTitleText);
     animateTitle('FULL STACK DEVELOPER (MERN)', setFsdText);
 
     const intervalId = setInterval(() => {
-      animateTitle("Hi, I'm VISHNU", setTitleText);
+      animateTitle("VISHNU", setTitleText);
       animateTitle('FULL STACK DEVELOPER (MERN)', setFsdText);
     }, 9000);
 
@@ -29,7 +29,7 @@ function Intro() {
   return (
     <section id='intro'>
       <div id='left-content'>
-        <h1 id='title'>{titleText}</h1><br />
+        <h1 id='title'>Hi, I'm {titleText}</h1><br />
         <h1 id='fsd'>{fsdText}</h1>
       </div>
       <div id='right-content'>
