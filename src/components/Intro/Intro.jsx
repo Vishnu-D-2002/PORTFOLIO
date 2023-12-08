4001c914ee610336989714427b7b80efcc249177
Vishnu-D-2002 <vishnulosangels@gmail.com>
2023-12-08 13:58:19 +0530

    letters

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,16 +1,38 @@
-import React from 'react'
-import './Intro.css'; 
+import React, { useEffect } from 'react';
+import './Intro.css';
+
 function Intro() {
+  useEffect(() => {
+    const animateTitle = (id) => {
+      const title = document.getElementById(id);
+      const text = title.innerText;
+      title.innerHTML = ''; 
+
+      text.split('').forEach((letter, index) => {
+        setTimeout(() => {
+          const span = document.createElement('span');
+          span.textContent = letter;
+          title.appendChild(span);
+        }, index * 300);
+      });
+    };
+
+    animateTitle('title');
+    animateTitle('fsd');
+  }, []);
+
   return (
     <section id='intro'>
-      <div id='left'>
-        <h1 >Hi i am VISHNU</h1>
+      <div id='content'>
+        <h1 id='title'>Hi I am VISHNU</h1><br />
+        <h1 id='fsd'>FULL STACK DEVELOPER</h1><br />
+        <h3></h3>
       </div>
-    <div className='pro'>
-      <img src="/images/vishnu.png" alt="" className='rounded-image'/>
-    </div>
-  </section>
-  )
+      <div id='end'>
+        <img src="/images/vishnu.png" alt="" className='rounded-image' />
+      </div>
+    </section>
+  );
 }
 
 export default Intro;
