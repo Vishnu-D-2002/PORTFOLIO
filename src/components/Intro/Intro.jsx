cf196dadfcbface579c806238ca1a9e9eeb0f450
Vishnu-D-2002 <vishnulosangels@gmail.com>
2024-03-08 09:38:39 +0530

    button coloring

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -28,24 +28,29 @@ function Intro() {
 
   const handleResumeClick = () => {
     const resumeUrl =
-      "https://drive.google.com/file/d/1HbWZvYGpEBhhcd3M5s8zGrWJQF_uew-u/view";
+      "https://drive.google.com/file/d/1KL6bDFg7O7D1ZetZe8motBBgzBgwV349/view?usp=drivesdk";
     
     window.open(resumeUrl, '_blank'); 
   };
 
   return (
-    <section id='intro'>
-      <div id='left-content'>
-        <h1 id='title'>Hello , I'm {titleText}</h1><br />
-        <h1 id='fsd'>{fsdText}</h1>
-        <p id='para'>
-            I'm very Interested in creating websites and apps using a technology stack called MERN (MongoDB, Express.js, React, Node.js). Let's create something awesome together!
+    <section id="intro">
+      <div id="left-content">
+        <h1 id="title">Hello , I'm {titleText}</h1>
+        <br />
+        <h1 id="fsd">{fsdText}</h1>
+        <p id="para">
+          I'm very Interested in creating websites and apps using a technology
+          stack called MERN (MongoDB, Express.js, React, Node.js). Let's create
+          something awesome together!
         </p>
-        <button className='resume' onClick={handleResumeClick}>View / Download Resume</button>
+        <button className="glow-on-hover" onClick={handleResumeClick}>
+          View / Download Resume
+        </button>
       </div>
-      <div id='right-content'>
-        <div id='end'>
-          <img src="/images/vishnu.png" alt="" className='rounded-image' />
+      <div id="right-content">
+        <div id="end">
+          <img src="/images/vishnu.png" alt="" className="rounded-image" />
         </div>
       </div>
     </section>
